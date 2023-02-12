1e096dcde377ffe9ece384ad1786b6941c3aaf61
Arthur <arthurmrtl@gmail.com>
2023-02-12 20:31:31 +0100

    horse webpage

---
+++ cropper.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+def create_icon(path, filname,):
+    image = Image.open(path+'/'+filename)
+    new_size = (2613, 3934)
+    image.thumbnail(new_size, Image.ANTIALIAS)
+    image = image.convert("RGB")
+    name = filname.split('.')
+    image.save(path+'/icon/'+name[0]+'_crop.'+name[1], "JPEG", quality=30)
+    
+def compresor(path, filname,):
+    image = Image.open(path+'/'+filename)
+    image = image.convert("RGB")
+    image.save(path+'/'+filname, "JPEG", quality=70)
+
+
+dir = 'D:/Programme/arthurmrtl.github.io/images/horse'
+for filename in os.listdir(dir):
+    if(filename != 'icon'):
+        create_icon(dir,filename)
+    
