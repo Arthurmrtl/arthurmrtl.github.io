48191e395f704d22abc1384e86a6156e21d69b95
Arthur <arthurmrtl@gmail.com>
2023-04-07 18:23:14 +0200

    pdlv

--- cropper.py
+++ cropper.py
@@ -15,7 +15,7 @@ def compresor(path, filname,):
     image.save(path+'/'+filname, "JPEG", quality=70)
 
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/kevin'
+dir = 'D:/Programme/arthurmrtl.github.io/images/pdlv'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         compresor(dir,filename)
--- filler.py
+++ filler.py
@@ -3,25 +3,25 @@ import os
 import re
 htmlToAdd =''
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/car'
+dir = 'D:/Programme/arthurmrtl.github.io/images/pdlv'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         fileCrop = filename.split('.')
         htmlToAdd = htmlToAdd + """
-    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 item" data-aos="fade" data-src=images/car/"""+filename+""" ">
-        <a href="#"><img src="images/car/icon/"""+fileCrop[0]+'_crop.'+fileCrop[1]+""" "class="img-fluid"></a>
+    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 item" data-aos="fade" data-src=images/pdlv/"""+filename+""" ">
+        <a href="#"><img src="images/pdlv/icon/"""+fileCrop[0]+'_crop.'+fileCrop[1]+""" "class="img-fluid"></a>
     </div>"""
 
 base = os.path.dirname(os.path.abspath(__file__))
 
-html = open(os.path.join(base, 'D:/Programme/arthurmrtl.github.io/car.html'))
+html = open(os.path.join(base, 'D:/Programme/arthurmrtl.github.io/car_pdlv.html'))
 
 soup = BeautifulSoup(html, 'html.parser')
 
 div = soup.find("div", {"id": "last"})
 div.insert_after(htmlToAdd)
 
-with open('D:/Programme/arthurmrtl.github.io/car.html', "wb") as f_output:
+with open('D:/Programme/arthurmrtl.github.io/car_pdlv.html', "wb") as f_output:
     f_output.write(soup.prettify("utf-8",formatter=None))
 
 
