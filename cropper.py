4e8c77dff1a2b3650b1c5bbd4a1fef9a014015e8
Arthur <arthurmrtl@gmail.com>
2023-07-14 20:38:10 +0200

    add monza

--- cropper.py
+++ cropper.py
@@ -15,7 +15,7 @@ def compresor(path, filname,):
     image.save(path+'/'+filname, "JPEG", quality=70)
 
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/swift'
+dir = 'D:/Programme/arthurmrtl.github.io/images/car_2'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         compresor(dir,filename)
