2c55ca6ccfda95613549ff5d7d7d6fdd1a68560a
Arthur <arthurmrtl@gmail.com>
2023-07-22 20:35:20 +0200

    anymania add

--- cropper.py
+++ cropper.py
@@ -15,7 +15,7 @@ def compresor(path, filname,):
     image.save(path+'/'+filname, "JPEG", quality=70)
 
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/anymania'
+dir = 'D:/Programme/arthurmrtl.github.io/images/tmp'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         compresor(dir,filename)
