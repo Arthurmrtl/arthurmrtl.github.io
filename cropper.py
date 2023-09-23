73fe6ee12571fa4a441f39275267b76d3c59081e
Arthur <arthurmrtl@gmail.com>
2023-09-23 20:28:51 +0200

    horse

--- cropper.py
+++ cropper.py
@@ -15,7 +15,7 @@ def compresor(path, filname,):
     image.save(path+'/'+filname, "JPEG", quality=70)
 
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/tmp'
+dir = 'D:/Programme/arthurmrtl.github.io/images/horse'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         compresor(dir,filename)
