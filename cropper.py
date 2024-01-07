81ae15feaa87673253335eb0c87e254a7868848c
Arthur <arthurmrtl@gmail.com>
2024-01-07 15:48:48 +0100

    montagne add

--- cropper.py
+++ cropper.py
@@ -15,7 +15,7 @@ def compresor(path, filname,):
     image.save(path+'/'+filname, "JPEG", quality=70)
 
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/clio'
+dir = 'D:/Programme/arthurmrtl.github.io/images/montagne'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         compresor(dir,filename)
