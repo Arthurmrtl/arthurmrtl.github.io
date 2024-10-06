faa65fb97dfab99c91e3ec29380c5646df62133a
Arthur <arthurmrtl@gmail.com>
2024-10-06 14:45:07 +0200

    horse news

--- filler.py
+++ filler.py
@@ -3,25 +3,25 @@ import os
 import re
 htmlToAdd =''
 
-dir = 'D:/Programme/arthurmrtl.github.io/images/350z'
+dir = 'D:/Programme/arthurmrtl.github.io/images/horse'
 for filename in os.listdir(dir):
     if(filename != 'icon'):
         fileCrop = filename.split('.')
         htmlToAdd = htmlToAdd + """
-    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 item" data-aos="fade" data-src=images/350z/"""+filename+""" >
-        <a href="#"><img src="images/350z/icon/"""+fileCrop[0]+'_crop.'+fileCrop[1]+""" "class="img-fluid"></a>
+    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 item" data-aos="fade" data-src=images/horse/"""+filename+""" >
+        <a href="#"><img src="images/horse/icon/"""+fileCrop[0]+'_crop.'+fileCrop[1]+""" "class="img-fluid"></a>
     </div>"""
 
 base = os.path.dirname(os.path.abspath(__file__))
 
-html = open(os.path.join(base, 'D:/Programme/arthurmrtl.github.io/350z.html'))
+html = open(os.path.join(base, 'D:/Programme/arthurmrtl.github.io/horse.html'))
 
 soup = BeautifulSoup(html, 'html.parser')
 
 div = soup.find("div", {"id": "last"})
 div.insert_after(htmlToAdd)
 
-with open('D:/Programme/arthurmrtl.github.io/350z.html', "wb") as f_output:
+with open('D:/Programme/arthurmrtl.github.io/horse.html', "wb") as f_output:
     f_output.write(soup.prettify("utf-8",formatter=None))
 
 
